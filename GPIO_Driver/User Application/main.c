4ce8bf570833fa17f4353ffe979164f7728a0fc3
fal-e <ss322u@gmail.com>
2019-08-24 00:47:48 +0930

    Cleaned up Main.c

--- GPIO_Driver/User Application/main.c
+++ GPIO_Driver/User Application/main.c
@@ -23,49 +23,38 @@
 **===========================================================================
 */
 
+/* READ IMPORTANT
+ *
+ * The code below contains an three examples.
+ * - It shows how to set up pins. For example how to set up the on-board LED and an analog pin.
+ * - Shows how to use the custom I2C functions
+ * - Shows how to use ADC and print to an 16 x 02 LCD screen via I2C
+ *
+ */
 
 
-int main(void)
-{
+int main(void) {
 
-/*config_clock_50MHz(); // Configuring the delays, as well as setting the clk freq
-lcd_init();
-lcd_goto_xy(2,1);
-lcd_print("Suad");
-lcd_goto_xy(4,2);
-lcd_print("Shahini");
-//delay_mS(2000);
-*/
+    config_clock_50MHz();
 
- config_clock_50MHz();
+    // Setting up PORT C Pin 0 as an analog input
+    GPIO_TYPE myGPIO;
+    myGPIO.port = PORTC;
+    myGPIO.mode = ANALOG_MODE;
+    gpio_port_config(myGPIO,(int []) {0},1); // 0 is the Pin number, 1 is the number of pins you want to configure
+    // It is possible to setup multiple pins with the function above.
 
+    /* Example below is how to set up the STM32F411RE on board LED.
+     * It is not */
+    GPIO_TYPE myLed;
+    myLed.port = PORTA;
+    myLed.mode = OUTPUT_MODE;
+    myLed.mode_type = OUTPUT_PUP;
+    gpio_port_config(myLed,(int []) {5},1);
 
-GPIO_TYPE myGPIO;
-myGPIO.port = PORTC;
-myGPIO.mode = ANALOG_MODE;
-gpio_port_config(myGPIO,(int []) {0},1);
+    I2C_enable(); // Setting up I2C
 
-
-GPIO_TYPE myLed;
-myLed.port = PORTA;
-myLed.mode = OUTPUT_MODE;
-myLed.mode_type = OUTPUT_PUP;
-gpio_port_config(myLed,(int []) {5},1);
-
-
-
-
-I2C_enable();
-//lcd_i2c_clear();
-//lcd_i2c_sendCmd(0b1100);
-
-// Read data sheet for the GPIO expander in STM32 folder.
-// The first data bit sent to the GPIO expander is whether it is input/output etc.
-
-
-//lcd_i2c_init();
-
-
+/* Printing to LCD via I2C Example: */
 
 /*for (int i = 0; i < 50; i++) {
 	lcd_i2c_print("Suad");
@@ -73,105 +62,36 @@ I2C_enable();
 	lcd_i2c_clear();
 	//delay_mS(500);
 	lcd_i2c_goto_xy(1,2);
-	lcd_i2c_print("The King");
+	lcd_i2c_print("Shahini");
 	delay_mS(250);
 	lcd_i2c_clear();
 
-}
-*/
-//char str[16];
-
-//while(1){
-//lcd_i2c_print("Value: ");
-
-//I2C_scan();
-uint16_t result = imu_accel_x();
-//itoa(result, str, 10); // convert from integer to string
+} */
 
 
+    char str[16];
+    // Setting up ADC and printing it to the LCD via I2C
+    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+    ADC1->SQR1 |= ADC_SQR1_L_0; // One conversion ->  0001. 1111
+    ADC1->SQR3 |= ADC_SQR3_SQ1_1 | ADC_SQR3_SQ1_3; // 2 or 10, gives us 1100 which is 10. We want
+    ADC1->CR2 |= ADC_CR2_CONT; // continous mode
+    ADC1->CR2 |= ADC_CR2_ADON;
+    // maybe put a check that adon is 1
+    ADC1->CR2 |= ADC_CR2_SWSTART; // Continous Conversion Mode
 
-//lcd_i2c_print(str);
-//lcd_i2c_print("V");
-//delay_mS(1000);
-//lcd_i2c_clear();
-//}
+    while(1) {
+        int result = ADC1->DR;
+        float new_result = result/819.0f;
+        snprintf(str,6,"%f", new_result); //snprintf not supported in c99...?
+        //ftoa(new_result, 3, str);
+        //itoa(result, str, 10); // convert from integer to string
+        lcd_i2c_print("Voltage: ");
+        lcd_i2c_print(str);
+        lcd_i2c_print("V");
+        delay_mS(1000);
+        lcd_i2c_clear();
+    }
 
-
-
-
-
-
-
-
-
-/*  ADC CODE
-
-char str[16]; // Temporary placement
-
-
-RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
-ADC1->SQR1 |= ADC_SQR1_L_0; // One conversion ->  0001. 1111
-ADC1->SQR3 |= ADC_SQR3_SQ1_1 | ADC_SQR3_SQ1_3; // 2 or 10, gives us 1100 which is 10. We want
-ADC1->CR2 |= ADC_CR2_CONT; // continous mode
-ADC1->CR2 |= ADC_CR2_ADON;
-// maybe put a check that adon is 1
-ADC1->CR2 |= ADC_CR2_SWSTART; // Continous Conversion Mode
-
-while(1){
-int result = ADC1->DR;
-float new_result = result/819.0f;
-snprintf(str,6,"%f", new_result); //snprintf not supported in c99...?
-//ftoa(new_result, 3, str);
-//itoa(result, str, 10); // convert from integer to string
-lcd_i2c_print("Voltage: ");
-lcd_i2c_print(str);
-lcd_i2c_print("V");
-delay_mS(1000);
-lcd_i2c_clear();
-}
-*/
-
-
-
-
-
-
-// ADC on/off
-// ADON in ADC_CR2 register.
-
-// Conversion start
-// SWSTART or JSWSTART is set
-
-// Conversion stop clear ADON bit
-
-// ADCLK is generated from APB2 clock. Ours is 50 MHz.
-// Programmable prescalar allowed e.g. /2 /2 /6 /8
-
-
-// We want to read the temperature and display it on the LCD.
-
-
-
-// Need to add code that checks for acknowledge. If not acknowledged then skip the code below,
-// otherwise run it.
-
-//while(!(I2C1->SR1 & I2C_SR1_ADDR)); // Wait for Received Address matched
-
-//while(!(I2C1->SR2 & I2C_SR2_BUSY));
-
-
-
-
-
-
-// TODO: Code that handles NACK from slave.
-// The program is currently stuck in an infinite loop in the I2C_data command
-// Specifically: while(!(I2C1->SR1 & I2C_SR1_TXE));
-// This is because when a NACK is received (e.g. wrong address), TXE is not set, therefore remaining in the loop.
-// Need to fix this by adding an interrupt, which STOPs or restarts communication.
-// Otherwise, will be stuck in loop and no more communication can continue.
-
-//
 }
 
 
